63eb89a7e023d48ffdb2b0571cfc128724010fcf
Michelle Macaraeg <macaraegm@gmail.com>
2020-11-17 14:44:31 -0800

    setup rebuild at restart

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I'm the home component</div>;
+  return <div>I'm the VERY VERY BEST home component</div>;
 };
 
 export default Home;
+
+
+// 1st Terminal - npm run dev:server
+// 2nd Terminal - npm run dev:build:server
