51fc9be080d3278dd82281ee00185f2c0d2aec14
Michelle Macaraeg <macaraegm@gmail.com>
2020-11-19 12:48:32 -0800

    Updated code to debug

--- webpack.client.js
+++ webpack.client.js
@@ -1,8 +1,8 @@
 const path = require('path');
-const merge = require('webpack.merge');
+const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js')
 
-module.exports = {
+const config = {
   // Tell webpack the root file of our
   // server application
   entry: './src/client/client.js',
